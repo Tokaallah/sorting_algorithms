348c3ff42b35fea9172bde1beccd0c1d2f8b56bc
Tokaallah <tokallah.92@gmail.com>
2023-06-25 03:35:44 -0700

    [Feature]: bubble sort

--- sort.h
+++ sort.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,6 +23,13 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+/* aux functions */
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void swap_bubble(int *a, int *b);
+
 
 /* algorithm functions */
 
